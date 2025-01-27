51b58f7b0c2394659e38a9f6d91e5ffc25986666
viniaquino <contato.viniaquino@outlook.com>
2025-01-27 15:24:02 -0300

    refactor: alteração na exibição do card

--- src/components/categorias/cardcategorias/CardCategorias.tsx
+++ src/components/categorias/cardcategorias/CardCategorias.tsx
@@ -8,11 +8,12 @@ interface CardCategoriasProps{
 function CardCategorias({ categoria }: CardCategoriasProps) {
     return (
         <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-            <header className='py-2 px-6 bg-teal-500 text-white font-bold text-2xl'>
+            <header className='py-1 px-6 bg-teal-500 text-white font-bold text-2xl'>
                 Categoria
             </header>
-            <p className='p-8 text-3xl bg-slate-200 h-full'>{categoria.descricao}</p>
-            
+            <p className='p-4 text-2xl font-semibold text-gray-800 bg-slate-200'>{categoria.nome}</p>
+            <p className='p-4 text-lg text-gray-600 bg-slate-200'>{categoria.descricao}</p>
+        
             <div className="flex">
                 <Link to={`/editarcategoria/${categoria.id}`} 
                     className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 
@@ -21,13 +22,13 @@ function CardCategorias({ categoria }: CardCategoriasProps) {
                 </Link>
 
                 <Link to={`/deletarcategoria/${categoria.id}`} 
-                        className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
+                    className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
                     flex items-center justify-center'>
                     <button>Deletar</button>
                 </Link>
-            </div>
+         </div>
+    </div>
 
-        </div>
     )
 }
 
